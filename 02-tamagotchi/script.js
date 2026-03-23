e669c50eeda50fac8e17ac49f3b816302d313583
migueldinois <dinoismiguel2@gmail.com>
2026-03-23 20:48:20 -0300

    feat(pet): implementar lógica de brincar e definição de nome com validação

--- 02-tamagotchi/script.js
+++ 02-tamagotchi/script.js
@@ -1,22 +1,32 @@
 // Elementos
 // Botoes
-const btnAlimentar = document.querySelector('.btn-alimentar')
-const btnBrincar = document.querySelector('.btn-brincar')
-const btnStatus = document.querySelector('.btn-status')
+const btnAlimentar = document.querySelector('#btn-alimentar')
+const btnBrincar = document.querySelector('#btn-brincar')
+const btnStatus = document.querySelector('#btn-status')
+const btnDefinirNome = document.querySelector('#btn-definir-nome')
 // Fome
-const valorFome = document.querySelector('.valor-fome')
-const barraFome = document.querySelector('.barra-fome')
+const valorFome = document.querySelector('#valor-fome')
+const barraFome = document.querySelector('#barra-fome')
 // Felicidade
-const valorFelicidade = document.querySelector('.valor-Felicidade')
-const barraFelicidade = document.querySelector('.barra-Felicidade')
+const valorFelicidade = document.querySelector('#valor-Felicidade')
+const barraFelicidade = document.querySelector('#barra-Felicidade')
+
+const mensagemPet = document.querySelector('#mensagem-pet')
+const inputNome = document.querySelector('#input-nome')
+const displayNome = document.querySelector('.display__nome')
 
 
 
+const atualizarBarraValor = () => {
+    // Fome
+    valorFome.style.width = pet.fome
+    barraFome.style.width = pet.fome + '%';
 
+    // Felicidade
+    valorFelicidade.style.width = pet.felicidade
+    barraFelicidade.style.width - pet.felicidade + '%';
 
-
-
-
+}
 
 const pet = {
     nome: undefined,
@@ -24,20 +34,45 @@ const pet = {
     felicidade: 50,
     energia: 100,
 
-    
     alimentar() {
         this.fome -= 15
-        if (this.fome < 0){
-            this.fome = 0
-        }
+
         this.felicidade += 5
-        if (this.felicidade > 100){
+        if (this.felicidade > 100) {
             this.felicidade = 100
         }
         this.energia -= 5
-        if (this.energia < 0){
+        if (this.energia < 0) {
             this.energia = 0
         }
+    },
+
+    brincar() {
+        this.felicidade += 20
+        this.fome += 10
+        if (this.energia > 20) {
+            this.energia -= 15
+        } else if (this.energia < 20) {
+            mensagemPet.textContent = 'Estou muito cansado para brincar!'
+        }
+
     }
 }
 
+
+btnBrincar.addEventListener('click', () => {
+    pet.brincar()
+    console.log(pet)
+    atualizarBarraValor()
+
+})
+
+btnDefinirNome.addEventListener('click', () => {
+    if (inputNome.value === '') {
+        mensagemPet.textContent = 'Você deve preencher um nome para o seu bixinho!'
+    } else {
+        pet.nome = inputNome.value
+        displayNome.textContent = pet.nome
+    }
+
+})
